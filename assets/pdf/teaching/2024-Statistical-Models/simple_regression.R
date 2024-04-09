b3aa6cd5b0b7f3ac76997351e43529ce53b2707d
sfanzon <126147228+sfanzon@users.noreply.github.com>
2024-04-09 17:59:24 +0000

    Deploying to gh-pages from @ sfanzon/sfanzon.github.io@db5d88f54bfa4371031f475e9fb5888f4797e460 🚀

---
+++ assets/pdf/teaching/2024-Statistical-Models/simple_regression.R
@@ -0,0 +1,23 @@
+# Remember to save data on Desktop
+
+# Change working directory to Desktop
+# In MacOS type
+# setwd("~/Desktop")
+
+# In Windows type
+# setwd("C:/Users/YourUsername/Desktop")
+
+# Read the data
+prices <- read.table(file = "stock_gold.txt",
+                     header = TRUE
+                     )
+
+# Store data into vectors
+stock.price <- prices$stock_price
+gold.price <- prices$gold_price
+
+# Fit simple linear regression model
+fit.model <- lm(gold.price ~ stock.price)
+
+# Print result to screen
+print(summary(fit.model))
