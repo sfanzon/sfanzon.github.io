c661365871bfd083f4ca5410e38b3df53aea90d9
sfanzon <126147228+sfanzon@users.noreply.github.com>
2025-03-05 11:14:09 +0000

    Deploying to gh-pages from @ sfanzon/sfanzon.github.io@707f5fe1e96f469f4408fe076fd40ae170a8dca0 🚀

--- assets/pdf/teaching/2025-Statistical-Models/monte_carlo_pi.R
+++ assets/pdf/teaching/2025-Statistical-Models/monte_carlo_pi.R
@@ -1,14 +1,14 @@
-N <- 10000
+N <- 10000      # We do 100000 iterations and print every 10000
 total <- 0      # Counts total number of points
 in_circle <- 0  # Counts points falling in circle
 
-for (j in 1:10) {
-  for (i in 1:N) {
+for (j in 1:10) {   # This loop is for printing message every N iterations
+  for (i in 1:N) {  # Loop in which points are counted
     x <- runif(1, -1, 1); y <- runif(1, -1, 1);  # sample point (x,y)
     if (x^2 + y^2 <= 1) {    
       in_circle <- in_circle + 1  # If (x,y) in circle increase counter
     }
-    total <- total + 1  # Increase total counter
+    total <- total + 1  # Increase total counter (in any case)
   }
   
   pi_approx <- ( in_circle / total ) * 4  # Compute approximate area
