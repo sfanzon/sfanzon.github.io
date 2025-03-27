5a4e34a01d00d42983a9d37f0b4c95cdcf399b7f
sfanzon <126147228+sfanzon@users.noreply.github.com>
2025-03-27 15:59:14 +0000

    Deploying to gh-pages from @ sfanzon/sfanzon.github.io@57f809d1c23f0dd242894bd1dc8c10b3fadb3926 🚀

--- assets/pdf/teaching/2025-Statistical-Models/longley_regression.R
+++ assets/pdf/teaching/2025-Statistical-Models/longley_regression.R
@@ -8,19 +8,19 @@
 # setwd("C:/Users/YourUsername/Desktop")
 
 # Read data file
-longley <- read.table(file = "longley.txt",
-                      header = TRUE)
+longley <- read.table(file = "longley.txt", header = TRUE)
 
 # Store columns in vectors
-x2 <- longley[ , 1]
-x3 <- longley[ , 2]
-x4 <- longley[ , 3]
-x5 <- longley[ , 4]
-x6 <- longley[ , 5]
-y <- longley[ , 6]
+x2 <- longley[ , 1]        # GNP Deflator
+x3 <- longley[ , 2]        # GNP
+x4 <- longley[ , 3]        # Unemployed
+x5 <- longley[ , 4]        # Armed Forces
+x6 <- longley[ , 5]        # Population
+x7 <- longley[ , 6]        # Year
+y <- longley[ , 7]         # Employed
 
 # Fit multiple regression model
-model <- lm(y ~ x2 + x3 + x4 + x5 + x6)
+model <- lm(y ~ x2 + x3 + x4 + x5 + x6 + x7)
 
 # Print summary
 print(summary(model))
