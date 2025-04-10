10d17c3f760ae8a53465cecaef2ce4dd2530fa28
sfanzon <126147228+sfanzon@users.noreply.github.com>
2025-04-10 20:29:23 +0000

    Deploying to gh-pages from @ sfanzon/sfanzon.github.io@a6e5c65157500fb4c585b4cb06e16f87bdd42fd5 🚀

--- assets/pdf/teaching/2025-Statistical-Models/anova.R
+++ assets/pdf/teaching/2025-Statistical-Models/anova.R
@@ -8,32 +8,23 @@
 # setwd("C:/Users/YourUsername/Desktop")
 
 # Load dataset
-sales <- read.table(file = "fridge_sales.txt",
-                    header = TRUE)
+d <- read.table(file = "fridge_sales.txt", header = TRUE)
 
-# Assign data-frame columns to vectors
-fridge <- sales[ , 1]
-durables <- sales[ , 2]
-q1 <- sales[ , 3]
-q2 <- sales[ , 4]
-q3 <- sales[ , 5]
-q4 <- sales[ , 6]
-quarter <- sales[ , 7]
+# Convert d$quarter into a factor
+d$quarter <- factor(d$quarter)
+
+# Produce boxplot to compare sales in each quarter
+boxplot(fridge.sales ~ quarter, data = d,
+        main = "Quarterly Fridge sales", ylab = "Fridge sales")
+
+# Perform ANOVA F-test
+anova <- aov(fridge.sales ~ quarter, data = d)
+
+# Print output
+print(summary(anova))
 
 # Fit dummy variable regression model
 # We drop q1 to avoid dummy variable trap
-dummy.lm <- lm (fridge ~ q2 + q3 + q4)
+dummy <- lm (fridge.sales ~ q2 + q3 + q4, data = d)
 
-# Fit equivalent dummy variable regression model
-# Need to convert quarter to a factor
-quarter.f <- factor(quarter)
-factor.lm <- lm(fridge ~ quarter.f)
-
-print(summary(dummy.lm))
-print(summary(factor.lm))
-
-# Fit ANOVA model
-factor.aov <- aov(fridge ~ quarter.f)
-
-# Print output
-print(summary(factor.aov))
+print(summary(dummy))
